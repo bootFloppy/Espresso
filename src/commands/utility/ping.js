3f6690c6fa3cb03feeaa5b7913615616a50fa7ba
bootFloppy <neonisdaboi@outlook.com>
2024-12-19 23:17:59 -0600

    inital committing of the commits (balls)

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('pingpong')
+		.setDescription('this is not rhythm heaven'),
+	async execute(interaction) {
+		//await interaction.reply(':ping_pong: ${client.ws.ping}ms.');
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(`:ping_pong: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};
